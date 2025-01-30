fe70c0a4f0c36e656c111ed9c00f75e095c63e5e
dlee210 <rlee8517@g.ucla.edu>
2025-01-30 10:17:03 -0800

    initializing part3

---
+++ Project_2/pokedex_helper.py
@@ -0,0 +1,91 @@
+from datasets import load_dataset
+from transformers import CLIPProcessor, CLIPModel
+import numpy as np
+import pandas as pd
+from glob import glob
+from PIL import Image
+import matplotlib.pyplot as plt
+import clip
+import torch
+from tqdm import tqdm
+from scipy.special import softmax
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.manifold import TSNE
+
+# load csv file and image paths to construct pokedex, use type_to_load=None to load all types, else use a list of types 1 to load
+def construct_pokedex(csv_path='Pokemon.csv', image_dir='./images/', type_to_load=None):
+    pokedex = pd.read_csv(csv_path)
+    image_paths = []
+
+    for pokemon_name in pokedex["Name"]:
+        imgs = glob(f"{image_dir}/{pokemon_name}/0.jpg")
+        if len(imgs) > 0:
+            image_paths.append(imgs[0])
+        else:
+            image_paths.append(None)
+
+    pokedex["image_path"] = image_paths
+    pokedex = pokedex[pokedex["image_path"].notna()].reset_index(drop=True)
+
+    # only keep pokemon with distinct id
+    ids, id_counts = np.unique(pokedex["ID"], return_counts=True)
+    ids, id_counts = np.array(ids), np.array(id_counts)
+    keep_ids = ids[id_counts == 1]
+
+    pokedex = pokedex[pokedex["ID"].isin(keep_ids)].reset_index(drop=True)
+    pokedex["Type2"] = pokedex["Type2"].str.strip()
+    if type_to_load is not None:
+        pokedex = pokedex[pokedex["Type1"].isin(type_to_load)].reset_index(drop=True)
+    return pokedex
+
+# load clip model
+def load_clip_model():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-L/14", device=device)
+    return model, preprocess, device
+
+# inference clip model on a list of image path
+def clip_inference_image(model, preprocess, image_paths, device):
+    image_embeddings = []
+    with torch.no_grad():
+        for img_path in tqdm(image_paths):
+            img = Image.open(img_path)
+            img_preprocessed = preprocess(img).unsqueeze(0).to(device)
+            image_embedding = model.encode_image(img_preprocessed).detach().cpu().numpy()
+            image_embeddings += [image_embedding]
+            
+    image_embeddings = np.concatenate(image_embeddings, axis=0)
+    image_embeddings /= np.linalg.norm(image_embeddings, axis=-1, keepdims=True)
+    return image_embeddings
+
+# inference clip model on a list of texts
+def clip_inference_text(model, preprocess, texts, device):
+    with torch.no_grad():
+        text_embeddings = model.encode_text(clip.tokenize(texts).to(device)).detach().cpu().numpy()
+    text_embeddings /= np.linalg.norm(text_embeddings, axis=-1, keepdims=True)
+    return text_embeddings
+
+# compute similarity of texts to each image
+def compute_similarity_text_to_image(image_embeddings, text_embeddings):
+    similarity = softmax((100.0 * image_embeddings @ text_embeddings.T), axis=-1)
+    return similarity
+
+# compute similarity of iamges to each text
+def compute_similarity_image_to_text(image_embeddings, text_embeddings):
+    similarity = softmax((100.0 * image_embeddings @ text_embeddings.T), axis=0)
+    return similarity
+
+# Use TSNE to project CLIP embeddings to 2D space
+def umap_projection(image_embeddings, n_neighbors=15, min_dist=0.1, metric='cosine'):
+    distance_matrix = np.zeros((image_embeddings.shape[0], image_embeddings.shape[0]))
+    for i in range(image_embeddings.shape[0]):
+        for j in range(image_embeddings.shape[0]):
+            if i == j:
+                distance_matrix[i, j] = 1
+            else:
+                distance_matrix[i, j] = np.dot(image_embeddings[i], image_embeddings[j])
+    distance_matrix = 1 - distance_matrix
+    reducer = TSNE(n_components=2, metric="precomputed", init="random", random_state=42)
+    visualization_data = reducer.fit_transform(distance_matrix)
+    return visualization_data
