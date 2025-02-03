654e3ae6d344198e6390df62010b79c01c183334
cdbeon <chandlerbeon@berkeley.edu>
2025-02-03 15:40:06 -0800

    Part II Progress

--- Project_2/utils.py
+++ Project_2/utils.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score, adjusted_rand_score, adjusted_mutual_info_score
+from sklearn.metrics import homogeneity_score, completeness_score, v_measure_score, adjusted_rand_score, adjusted_mutual_info_score, rand_score
 import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tag import pos_tag
@@ -12,6 +12,12 @@ nltk.download('wordnet', quiet=True)
 nltk.download('averaged_perceptron_tagger', quiet=True)
 nltk.download('omw-1.4', quiet=True)
 nltk.download('averaged_perceptron_tagger_eng', quiet=True)
+
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, TensorDataset
 
 # Given function to parse out HTML-related characters
 def clean(text):
@@ -100,7 +106,8 @@ def evaluate_cluster_model(true_labels, predict_labels):
         'Completeness': [completeness_score(true_labels, predict_labels)],
         'V-measure': [v_measure_score(true_labels, predict_labels)],
         'Adjusted Rand Index': [adjusted_rand_score(true_labels, predict_labels)],
-        'Adjusted Mutual Information Score': [adjusted_mutual_info_score(true_labels, predict_labels)]
+        'Adjusted Mutual Information Score': [adjusted_mutual_info_score(true_labels, predict_labels)],
+        'Rand Score': [rand_score(true_labels, predict_labels)]
     })
     
     return eval_df
@@ -115,3 +122,68 @@ def get_svd_perc_ratio(n_components, df):
     https://stackoverflow.com/questions/41385708/multiprocessing-example-giving-attributeerror-in-jupyter-notebook
     """
     return (n_components, sum(TruncatedSVD(n_components=n_components).fit(df).explained_variance_ratio_))
+
+
+# Autoencoder Code for Part II
+mps = torch.device('mps')
+class Autoencoder(torch.nn.Module, TransformerMixin):
+    def __init__(self, n_components):
+        super().__init__()
+        self.n_components = n_components
+        self.n_features = None  # to be determined with data
+        self.encoder = None
+        self.decoder = None
+        
+    def _create_encoder(self):
+        return nn.Sequential(
+            nn.Linear(4096, 1280),
+            nn.ReLU(True),
+            nn.Linear(1280, 640),
+            nn.ReLU(True), nn.Linear(640, 120), nn.ReLU(True), nn.Linear(120, self.n_components))
+    
+    def _create_decoder(self):
+        return nn.Sequential(
+            nn.Linear(self.n_components, 120),
+            nn.ReLU(True),
+            nn.Linear(120, 640),
+            nn.ReLU(True),
+            nn.Linear(640, 1280),
+            nn.ReLU(True), nn.Linear(1280, 4096))
+    
+    def forward(self, X):
+        encoded = self.encoder(X)
+        decoded = self.decoder(encoded)
+        return decoded
+    
+    def fit(self, X):
+        X = torch.tensor(X, dtype=torch.float32, device='mps')
+        self.n_features = X.shape[1]
+        self.encoder = self._create_encoder()
+        self.decoder = self._create_decoder()
+        self.to(mps)
+        self.train()
+        
+        criterion = nn.MSELoss()
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3, weight_decay=1e-5)
+
+        dataset = TensorDataset(X)
+        dataloader = DataLoader(dataset, batch_size=128, shuffle=True)
+
+        for epoch in tqdm(range(100)):
+            for (X_,) in dataloader:
+                X_ = X_.to(mps)
+                # ===================forward=====================
+                output = self(X_)
+                loss = criterion(output, X_)
+                # ===================backward====================
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+        return self     
+        
+    def transform(self, X):
+        X = torch.tensor(X, dtype=torch.float32, device='mps')
+        self.eval()
+        with torch.no_grad():
+            return self.encoder(X).cpu().numpy()
